8f8844bd8ab356ef9eb1c3304c55ec44406287bc
hktrollgames <hktrollgames@gmail.com>
2021-10-14 20:38:07 +0900

    배경에 별 추가, 점수 추가

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text pointText;
     public MovingCube item;
+    public Transform baseCube;
     public int level;
     float cubeHeight;
     public float distance = 2.75f;
@@ -17,17 +20,50 @@ public class GameManager : MonoBehaviour
         cubeHeight = item.transform.localScale.y;
         item.gameObject.SetActive(false);
         nextColor = item.GetComponent<Renderer>()
-            .material.GetColor("_ColorTop");
+            .sharedMaterial.GetColor("_ColorTop");
         CreateCube();
+        topCubeTr = baseCube;
     }
     void Update()
     {
-        if(Input.anyKeyDown)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            BreakCube();
             CreateCube();
+        }
     }
+    private void BreakCube()
+    {
+        if (scaleModifyCubeTr == null) // 첫번째는 
+            return;
+
+        //// 스케일, 
+        //// 포지션,
+        Vector3 newCubeScale, newCubePos;
+        //currentCubeTr  이번에 만들어진 큐브 -> 이거랑 비교하면 안됨.
+        // lastCubeTr 와 이거전에 만들어진 큐브와 비교해야함.
+        newCubeScale = new Vector3(
+            topCubeTr.localScale.x - Mathf.Abs(scaleModifyCubeTr.localPosition.x - topCubeTr.localPosition.x)
+            , scaleModifyCubeTr.localScale.y
+            , topCubeTr.localScale.z - Mathf.Abs(scaleModifyCubeTr.localPosition.z - topCubeTr.localPosition.z)
+            );
+
+        newCubePos = Vector3.Lerp(scaleModifyCubeTr.position, topCubeTr.position, 0.5f) + Vector3.up * cubeHeight;
+
+        scaleModifyCubeTr.localScale = newCubeScale;
+        scaleModifyCubeTr.position = newCubePos;
+        scaleModifyCubeTr.GetComponent<MovingCube>().enabled = false;
+        scaleModifyCubeTr.name = "크기 줄인 큐브";
+    }
+
+    Transform topCubeTr;
+    Transform scaleModifyCubeTr;
+
     private void CreateCube()
     {
         level++;
+        pointText.text = level.ToString();
+
         // 홀 수 일때는 오른쪽, 짝 수 일때는 왼쪽
         Vector3 startPos;
         if(level % 2 == 1) // 홀수
@@ -38,8 +74,14 @@ public class GameManager : MonoBehaviour
         {
             startPos = new Vector3(-distance, level * cubeHeight, distance);
         }
+
         var newCube = Instantiate(item, startPos, item.transform.rotation);
+        newCube.transform.parent = item.transform.parent;
+        if (scaleModifyCubeTr != null)
+            newCube.pivot = scaleModifyCubeTr.transform.position;
+
         newCube.gameObject.SetActive(true);
+        newCube.name = level.ToString();
 
         // 다음 색 지정하자.
         Color.RGBToHSV(nextColor, out float h, out float s, out float v);
@@ -50,10 +92,11 @@ public class GameManager : MonoBehaviour
 
         newCube.GetComponent<Renderer>().material.SetColor("_ColorTop", nextColor);
         newCube.GetComponent<Renderer>().material.SetColor("_ColorBottom", nextColor);
-
         // 카메라 위로 이동하자.
         Camera.main.transform.Translate(0, cubeHeight, 0, Space.World);
 
+        topCubeTr = scaleModifyCubeTr;
+        scaleModifyCubeTr = newCube.transform;
     }
     public Gradient gradient;
 }
--- Assets/MovingCube.cs
+++ Assets/MovingCube.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
+    internal Vector3 pivot;
     Vector3 desPoint;    // 목표지점.
     Vector3 startPoint;  // 시작 지점.
     float startTime;
     void Start()
     {
-        startPoint = transform.position;
-        desPoint = new Vector3(-startPoint.x, startPoint.y, -startPoint.z);
+        startPoint = new Vector3(transform.position.x + pivot.x, transform.position.y, transform.position.z + pivot.z);
+        desPoint = new Vector3(-startPoint.x + pivot.x, startPoint.y, -startPoint.z + pivot.x);
         startTime = Time.time;
     }
     
