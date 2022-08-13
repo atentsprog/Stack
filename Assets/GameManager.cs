6ce250f2b439642d19213e7d27fdb355aefbb70f
chanseon.park <chanseon.park@kongstudios.net>
2022-08-13 20:09:55 +0900

    2021.3버전으로 수정

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -14,9 +14,15 @@ public class GameManager : MonoBehaviour
     public float distance = 2.75f;
     Color nextColor;
     public float colorChangeStep = 2f;
+    public Renderer bgRenderer;
+    Material bgMaterial;
+    public float bgColorOffsetTop = 0.2f;
+    public float bgColorOffsetBottom = 0.4f;
+
 
     void Start()
     {
+        bgMaterial = bgRenderer.material;
         cubeHeight = item.transform.localScale.y;
         item.gameObject.SetActive(false);
         nextColor = item.GetComponent<Renderer>()
@@ -45,15 +51,15 @@ public class GameManager : MonoBehaviour
     public bool isGameOver;
     private void BreakCube()
     {
-        if (scaleModifyCubeTr == null) // 첫번째는 
+        if (lastInClub == null) // 첫번째는 
             return;
 
         //// 스케일, 
         //// 포지션,
         //currentCubeTr  이번에 만들어진 큐브 -> 이거랑 비교하면 안됨.
         // lastCubeTr 와 이거전에 만들어진 큐브와 비교해야함.
-        float absOffsetX = Mathf.Abs(scaleModifyCubeTr.localPosition.x - topCubeTr.localPosition.x);
-        float absOffsetZ = Mathf.Abs(scaleModifyCubeTr.localPosition.z - topCubeTr.localPosition.z);
+        float absOffsetX = Mathf.Abs(lastInClub.localPosition.x - topCubeTr.localPosition.x);
+        float absOffsetZ = Mathf.Abs(lastInClub.localPosition.z - topCubeTr.localPosition.z);
 
         isGameOver = (topCubeTr.localScale.x < absOffsetX)
             || (topCubeTr.localScale.z < absOffsetZ);
@@ -68,7 +74,7 @@ public class GameManager : MonoBehaviour
 
         Vector3 newCubeScale = new Vector3(
             topCubeTr.localScale.x - absOffsetX
-            , scaleModifyCubeTr.localScale.y
+            , lastInClub.localScale.y
             , topCubeTr.localScale.z - absOffsetZ
             );
 
@@ -78,11 +84,11 @@ public class GameManager : MonoBehaviour
         //Debug.Break();
 
         // 제대로 드랍된 큐브 생성
-        CrateRemainCube(newCubeScale);
+        CrateInCube(newCubeScale);
 
 
         // 짤린 큐브 생성하자.
-        CreateDropCube(dropCubeScale, dropCubePos);
+        CreateOutCube(dropCubeScale, dropCubePos);
 
 
         // todo : 짤린 부분이 최소값보다 작다면 위치 스냅 시키고 콤보수치 올리자.
@@ -90,53 +96,53 @@ public class GameManager : MonoBehaviour
 
     private void OnGameOver()
     {
-        scaleModifyCubeTr.GetComponent<MovingCube>().enabled = false;
-        scaleModifyCubeTr.gameObject.AddComponent<Rigidbody>();
+        lastInClub.GetComponent<MovingCube>().enabled = false;
+        lastInClub.gameObject.AddComponent<Rigidbody>();
         pointText.text = "Game Over";
     }
 
     private void GetDropCubeScaleAndPosition(Vector3 newCubeScale, bool moveLocalX, out Vector3 dropCubeScale, out Vector3 dropCubePos)
     {
         dropCubeScale = new Vector3(
-    scaleModifyCubeTr.localScale.x - newCubeScale.x
-    , scaleModifyCubeTr.localScale.y
-    , scaleModifyCubeTr.localScale.z - newCubeScale.z
+    lastInClub.localScale.x - newCubeScale.x
+    , lastInClub.localScale.y
+    , lastInClub.localScale.z - newCubeScale.z
     );
 
         if (dropCubeScale.x < 0.001)
-            dropCubeScale.x = scaleModifyCubeTr.localScale.x;
+            dropCubeScale.x = lastInClub.localScale.x;
 
         if (dropCubeScale.z < 0.001)
-            dropCubeScale.z = scaleModifyCubeTr.localScale.z;
+            dropCubeScale.z = lastInClub.localScale.z;
 
 
-        bool isNegativePositionX = scaleModifyCubeTr.localPosition.x < topCubeTr.localPosition.x;
-        bool isNegativePositionZ = scaleModifyCubeTr.localPosition.z < topCubeTr.localPosition.z;
+        bool isNegativePositionX = lastInClub.localPosition.x < topCubeTr.localPosition.x;
+        bool isNegativePositionZ = lastInClub.localPosition.z < topCubeTr.localPosition.z;
         float directionX = isNegativePositionX ? 1 : -1;
         float directionZ = isNegativePositionZ ? -1 : 1;
         if (moveLocalX)
-            dropCubePos = scaleModifyCubeTr.localPosition - new Vector3(newCubeScale.x, 0, 0) * 0.5f * directionX;
+            dropCubePos = lastInClub.localPosition - new Vector3(newCubeScale.x, 0, 0) * 0.5f * directionX;
         else
-            dropCubePos = scaleModifyCubeTr.localPosition + new Vector3(0, 0, newCubeScale.z) * 0.5f * directionZ;
+            dropCubePos = lastInClub.localPosition + new Vector3(0, 0, newCubeScale.z) * 0.5f * directionZ;
     }
 
-    private void CrateRemainCube(Vector3 newCubeScale)
+    private void CrateInCube(Vector3 newCubeScale)
     {
-        Vector3 newCubePos = Vector3.Lerp(scaleModifyCubeTr.position, topCubeTr.position, 0.5f);
-        newCubePos.y = scaleModifyCubeTr.position.y;
+        Vector3 newCubePos = Vector3.Lerp(lastInClub.position, topCubeTr.position, 0.5f);
+        newCubePos.y = lastInClub.position.y;
 
-        scaleModifyCubeTr.localScale = newCubeScale;
-        scaleModifyCubeTr.position = newCubePos;
-        scaleModifyCubeTr.GetComponent<MovingCube>().enabled = false;
-        scaleModifyCubeTr.name = "크기 줄인 큐브";
+        lastInClub.localScale = newCubeScale;
+        lastInClub.position = newCubePos;
+        lastInClub.GetComponent<MovingCube>().enabled = false;
+        lastInClub.name = $"In:{level}";
     }
 
-    private void CreateDropCube(Vector3 dropCubeScale, Vector3 dropCubePos)
+    private void CreateOutCube(Vector3 dropCubeScale, Vector3 dropCubePos)
     {
-        var dropGo = Instantiate(scaleModifyCubeTr.gameObject, dropCubePos, scaleModifyCubeTr.rotation, scaleModifyCubeTr.parent);
+        var dropGo = Instantiate(lastInClub.gameObject, dropCubePos, lastInClub.rotation, lastInClub.parent);
         dropGo.transform.localScale = dropCubeScale;
         dropGo.transform.localPosition = dropCubePos;
-        dropGo.name = "드랍될 큐브";
+        dropGo.name = $"Out:{level}";
         dropGo.AddComponent<Rigidbody>();
     }
 
@@ -144,7 +150,7 @@ public class GameManager : MonoBehaviour
     public int comboCount;
 
     Transform topCubeTr;
-    Transform scaleModifyCubeTr;
+    Transform lastInClub;
 
     private void CreateCube()
     {
@@ -163,10 +169,10 @@ public class GameManager : MonoBehaviour
 
         var newCube = Instantiate(item, startPos, item.transform.rotation);
         newCube.transform.parent = item.transform.parent;
-        if (scaleModifyCubeTr != null)
+        if (lastInClub != null)
         {
-            newCube.pivot = scaleModifyCubeTr.transform.localPosition;
-            newCube.transform.localScale = scaleModifyCubeTr.transform.localScale;
+            newCube.pivot = lastInClub.transform.localPosition;
+            newCube.transform.localScale = lastInClub.transform.localScale;
         }
 
         newCube.gameObject.SetActive(true);
@@ -174,18 +180,27 @@ public class GameManager : MonoBehaviour
 
         // 다음 색 지정하자.
         Color.RGBToHSV(nextColor, out float h, out float s, out float v);
-        nextColor = Color.HSVToRGB(h + 1f/256 * colorChangeStep, s, v);
+        float nextColorH = h + 1f / 256 * colorChangeStep;
+        nextColor = Color.HSVToRGB(nextColorH, s, v);
 
         //// 그라데이션 사용할꺼면 아래 로직 사용
         //nextColor = gradient.Evaluate( level % 100f * 0.01f);
 
         newCube.GetComponent<Renderer>().material.SetColor("_ColorTop", nextColor);
         newCube.GetComponent<Renderer>().material.SetColor("_ColorBottom", nextColor);
+        float bgTopColorH = nextColorH + bgColorOffsetTop;
+        if (bgTopColorH > 1)
+            bgTopColorH = bgTopColorH - 1;
+        float bgBottomColorH = nextColorH + bgColorOffsetBottom;
+        if (bgBottomColorH > 1)
+            bgBottomColorH = bgBottomColorH - 1;
+        bgMaterial.SetColor("_ColorTop", Color.HSVToRGB(bgTopColorH, s, v));
+        bgMaterial.SetColor("_ColorBottom", Color.HSVToRGB(bgBottomColorH, s, v));
         // 카메라 위로 이동하자.
         Camera.main.transform.Translate(0, cubeHeight, 0, Space.World);
 
-        topCubeTr = scaleModifyCubeTr;
-        scaleModifyCubeTr = newCube.transform;
+        topCubeTr = lastInClub;
+        lastInClub = newCube.transform;
         level++;
     }
     public Gradient gradient;
